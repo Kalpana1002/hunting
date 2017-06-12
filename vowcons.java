3d55811aca872983aee0cd983e1372f9b9f634d3
S Kalpana <kalpanasathish2@gmail.com>
2017-06-13 00:22:36 +0530

    vowcons.java

---
+++ vowcons.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class vowcons
+{
+public static void main(String args[])
+{
+char ch=System.in.read();
+if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o' || ch=='u')
+{
+System.out.println("The character is vowel");
+else
+System.out.println("The character is consonant");
+}
+}
+}
