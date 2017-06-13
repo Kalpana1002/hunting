e56fd80ab2462e13d731567535cfe2ee7ae55b0b
S Kalpana <kalpanasathish2@gmail.com>
2017-06-13 12:19:21 +0530

    oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -6,8 +6,8 @@ public static void main(String args[])
 {
 int num=Integer.parseInt(args[0]);
 if(num%2==0)
-System.out.println("The number"+num+"is even");
+System.out.println("The number"+" "+num+" "+"is even");
 else
-System.out.println("The number"+num+"is odd");
+System.out.println("The number"+" "+num+" "+"is odd");
 }
 }
