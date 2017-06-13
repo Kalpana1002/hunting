f4b577990170d1f7c302e6d638b018af4137afa8
S Kalpana <kalpanasathish2@gmail.com>
2017-06-13 12:12:43 +0530

    posneg.java

--- posneg.java
+++ posneg.java
@@ -9,10 +9,8 @@ if(num>0)
 System.out.println("The number is positive");
 else if(num<0)
 System.out.println("The number is negative");
-else if(num==0)
+else 
 System.out.println("The number is zero");
-else
-System.out.println("The given input is not valid");
 }
 }
 
