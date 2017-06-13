b4686b82a51736168a4bd18896c608444c3d4263
S Kalpana <kalpanasathish2@gmail.com>
2017-06-13 12:26:37 +0530

    Create oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -5,9 +5,11 @@ class oddeven
 public static void main(String args[])
 {
 int num=Integer.parseInt(args[0]);
-if(num%2==0)
+if(num%2==0 && num>0)
 System.out.println("The number"+" "+num+" "+"is even");
-else
+else if(num%2!=0 && num>0)
 System.out.println("The number"+" "+num+" "+"is odd");
+else
+System.out.println("The number"+" "+num+" "+"is negative");   
 }
 }
