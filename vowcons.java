358e9a5beb8c615603e84f1df17fc40bccb37d3c
S Kalpana <kalpanasathish2@gmail.com>
2017-06-14 12:15:29 +0530

    vowcons.java

--- vowcons.java
+++ vowcons.java
@@ -9,7 +9,9 @@ char ch=sc.next().charAt(0);
 if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o' || ch=='u')
 {
 System.out.println("The character is vowel");
+}  
 else
+{  
 System.out.println("The character is consonant");
 }
 }
