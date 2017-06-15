ea6e9b571bd4d4e4119f3ff2cb3e259b17dce7b7
S Kalpana <kalpanasathish2@gmail.com>
2017-06-15 15:55:05 +0530

    alpha.java

---
+++ alpha.java
@@ -0,0 +1,16 @@
+# hunting
+codes
+import java.io.*;
+import java.util.*;
+class alpha
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char ch=sc.next().charAt(0);
+if(Character.isLetter(ch))
+System.out.println("The Character is alphabet");
+else
+System.out.println("The Character is not an alphabet");
+}
+}
