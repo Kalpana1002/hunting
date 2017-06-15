49278b960f40e2e5444e74d388dabf382b1b69b7
S Kalpana <kalpanasathish2@gmail.com>
2017-06-15 15:14:26 +0530

    vowcons.java

--- vowcons.java
+++ vowcons.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);  
 char ch=sc.next().charAt(0);
-if(ch>=1 && ch<=9)
+if(ch>='0' && ch<='9')
 {
 System.out.println("The given input is not valid");
 }
