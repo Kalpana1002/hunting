37e5b8713198d61052b4943c239d585244d1124c
S Kalpana <kalpanasathish2@gmail.com>
2017-06-15 15:09:36 +0530

    vowcons.java

--- vowcons.java
+++ vowcons.java
@@ -6,7 +6,11 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);  
 char ch=sc.next().charAt(0);
-if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o' || ch=='u')
+if(ch>=1 && ch<=9)
+{
+System.out.println("The given input is not valid");
+}
+else if(ch=='a' || ch=='e'|| ch=='i'|| ch=='o' || ch=='u')
 {
 System.out.println("The character is vowel");
 }  
