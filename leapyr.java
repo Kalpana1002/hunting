ee53a4f9ea71753812e8cb100ec198981b52238b
S Kalpana <kalpanasathish2@gmail.com>
2017-06-18 23:16:23 +0530

    leapyr.java

--- leapyr.java
+++ leapyr.java
@@ -6,8 +6,8 @@ public static void main(String args[])
 {
 int year=Integer.parseInt(args[0]);
 if((year%400==0)|| ((year%4==0) && (year%100!=0)))
-System.out.println(year+"is leap year");
+System.out.println(year+" "+is leap year");
 else
-System.out.println(year+"is not leap year");
+System.out.println(year+" "+is not leap year");
 }
 }
