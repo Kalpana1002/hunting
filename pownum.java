63536d822d1d316d05e9783e9b9bed9fe806adba
S Kalpana <kalpanasathish2@gmail.com>
2017-07-03 12:26:44 +0530

    pownum.java

---
+++ pownum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class pownum
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+double power=Math.pow(a,b);
+System.out.println(power);
+}
+}
