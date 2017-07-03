9f4c7158e2be2643bcd2577c9844bf1674857d5c
S Kalpana <kalpanasathish2@gmail.com>
2017-07-03 14:00:05 +0530

    eveninterval.java

---
+++ eveninterval.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class eveninterval
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+for(int i=num1;i<num2;i++)
+{
+if(i%2==0)
+System.out.print(i+" ");
+}
+}
+}
